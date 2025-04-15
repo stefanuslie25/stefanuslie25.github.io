3ebf81ef1d27825d5353c43a089ee41c2b73f912
stefanuslie25 <apasih.gatau.ah@gmail.com>
2025-04-15 18:07:01 +0700

    Update script.js

--- styles/script.js
+++ styles/script.js
@@ -2,8 +2,12 @@ if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pa
 	window.location.replace('/');
 }
 
-if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pathname.endsWith('list.html')) {
-	window.location.replace(window.location.pathname.replace('list.html', 'list'));
+if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pathname.endsWith('projects.html')) {
+	window.location.replace(window.location.pathname.replace('projects.html', 'projects'));
+}
+
+if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pathname.endsWith('contact.html')) {
+	window.location.replace(window.location.pathname.replace('contact.html', 'contact'));
 }
 
 if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pathname.endsWith('credits.html')) {

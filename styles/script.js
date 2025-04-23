5c12b31744ddc37bcad16e12789793c253c8b333
stefanuslie25 <apasih.gatau.ah@gmail.com>
2025-04-23 18:22:18 +0700

    rename file
     file-location.js -> script.js

--- styles/script.js
+++ styles/script.js
@@ -9,3 +9,8 @@ if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pa
 if (window.location.hostname === 'www.stefanuslie25.my.id' && window.location.pathname.endsWith('info.html')) {
 	window.location.replace(window.location.pathname.replace('info.html', 'info'));
 }
+
+document.querySelectorAll('.box').forEach(box => {
+  const bgImage = box.getAttribute('data-bg');
+  box.style.backgroundImage = `url(${bgImage})`;
+});

aafa7547736f9503e3ebf737f3123e1e178719d7
stefanuslie25 <apasih.gatau.ah@gmail.com>
2025-10-20 20:38:32 +0700

    switch theme

---
+++ theme/theme.js
@@ -0,0 +1,12 @@
+const themeLink = document.getElementById('theme-style');
+const toggleBtn = document.getElementById('toggle-theme');
+
+// Load theme dari localStorage jika ada
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) themeLink.href = savedTheme;
+
+toggleBtn.onclick = () => {
+    const newTheme = themeLink.href.includes('light') ? 'theme/theme-dark.css' : 'theme/theme-light.css';
+    themeLink.href = newTheme;
+    localStorage.setItem('theme', newTheme); // Simpan tema
+};

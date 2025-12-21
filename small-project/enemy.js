17ac8a0e28761e9b3393879b1150533f6b5dd972
stefanuslie25 <apasih.gatau.ah@gmail.com>
2025-12-21 22:21:20 +0700

    update enemy info

--- small-project/enemy.js
+++ small-project/enemy.js
@@ -1,16 +1,66 @@
 const enemies = [
 	{
 		name: "Slime Blue",
-		hp: 50,
-		attack: 1,
-		giveXpMin: 1,
-		giveXpMax: 5
+		hp: 100,
+		damage: {
+			min: 0,
+			max: 2
+		},
+		giveXp: {
+			min: 2,
+			max: 10
+		},
+		giveMoney: {
+			min: 50,
+			max: 100
+		}
 	},
 	{
 		name: "Slime Green",
-		hp: 100,
-		attack: 1,
-		giveXpMin: 2,
-		giveXpMax: 7
+		hp: 150,
+		damage: {
+			min: 1,
+			max: 3
+		},
+		giveXp: {
+			min: 5,
+			max: 20
+		},
+		giveMoney: {
+			min: 75,
+			max: 200
+		}
+	},
+	{
+		name: "Slime Pink",
+		hp: 250,
+		damage: {
+			min: 2,
+			max: 5
+		},
+		giveXp: {
+			min: 10,
+			max: 50
+		},
+		giveMoney: {
+			min: 100,
+			max: 500
+		}
+	},
+	{
+		name: "Slime King",
+		hp: 1000,
+		damage: {
+			min: 0,
+			max: 7
+		},
+		giveXp: {
+			min: 50,
+			max: 120
+		},
+		giveMoney: {
+			min: 300,
+			max: 1000
+		}
 	}
 ]
